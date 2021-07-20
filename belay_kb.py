4b36cbcc5df3151892ddf968adfee6e90f28f975
Belay Hailu <85073527+Belay-ink@users.noreply.github.com>
2021-07-20 15:25:52 -0500

    Update belay_kb.py

--- belay_kb.py
+++ belay_kb.py
@@ -50,43 +50,43 @@ url_prod = "https://surf.service-now.com/api/now/table/kb_knowledge"
 def main():
 
  
-base_url = "https://uswpldevops01.corp.service-now.com:8443"
+  base_url = "https://uswpldevops01.corp.service-now.com:8443"
 
  # send a request using ADM cridentials to get the last sucessfull build
-latest_build_number = requests.get(base_url + "/job/osimage-management-centos/lastSuccessfulBuild/buildNumber", auth = HTTPBasicAuth(sys.argv[5], sys.argv[6]))
+  latest_build_number = requests.get(base_url + "/job/osimage-management-centos/lastSuccessfulBuild/buildNumber", auth = HTTPBasicAuth(sys.argv[5], sys.argv[6]))
 
  
 # print("response from latest build number: ", latest_build_number.text)
 
 
-console_text_url = base_url + "/job/osimage-management-centos/" + latest_build_number.text + "/consoleText"
+  console_text_url = base_url + "/job/osimage-management-centos/" + latest_build_number.text + "/consoleText"
 
 # print(console_text_url)
  #Get the consule text using the KB
-latest_build_console_text = requests.get(console_text_url, auth = HTTPBasicAuth(sys.argv[5], sys.argv[6]))
+  latest_build_console_text = requests.get(console_text_url, auth = HTTPBasicAuth(sys.argv[5], sys.argv[6]))
 
 # print("response from latest console text: ", latest_build_console_text.text)
 
 
 #Automation 
-html_string_for_post = latest_build_console_text.text.replace("\n", "<br/>").replace("�","<br/>").replace('"',"'")
+  html_string_for_post = latest_build_console_text.text.replace("\n", "<br/>").replace("�","<br/>").replace('"',"'")
 
  
 # print(html_string_for_post)
-final_payload = payload.replace("<REPLACED_TEXT>", html_string_for_post)
+  final_payload = payload.replace("<REPLACED_TEXT>", html_string_for_post)
 
-print(final_payload)
+  print(final_payload)
 
  #post the url using KB cridetials to servicenow page
-response = requests.post(url_test, data=final_payload.encode('utf-8'), headers={'Content-Type': 'application/json'}, auth=HTTPBasicAuth(sys.argv[3], sys.argv[4]))
+  response = requests.post(url_test, data=final_payload.encode('utf-8'), headers={'Content-Type': 'application/json'}, auth=HTTPBasicAuth(sys.argv[3], sys.argv[4]))
 
  
-print(response)
+  print(response)
 
 if __name__ == "__main__":
 
  
-main()
+  main()
 
 
 
