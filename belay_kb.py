ef590e20b557b3cd73376efe313bcabb173adfa6
Belay Hailu <85073527+Belay-ink@users.noreply.github.com>
2021-07-20 15:48:10 -0500

    Update belay_kb.py

--- belay_kb.py
+++ belay_kb.py
@@ -64,7 +64,7 @@ def main():
 
 
 #Automation 
-  html_string_for_post = latest_build_console_text.text.replace("\n", "<br/>").replace("�","<br/>")
+  html_string_for_post = latest_build_console_text.text.replace("\n", "<br/>").replace("�","<br/>").replace('"',"'")
 
  
 # print(html_string_for_post)
