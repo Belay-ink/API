62bb4bac4d0408a3899fcddc53cfc78ec9af2b48
medhachaitanya <medha_chaitanya@hotmail.com>
2021-07-21 19:12:14 -0400

    Changed the code to accommodate the KB POST

--- belay_kb.py
+++ belay_kb.py
@@ -1,6 +1,7 @@
-# Args 1/2 - Service Account for DevOps username/password
-# Args 3/4 - Knowledge Base API username/password
-# Args 5/6 - Service Account for Jenkins username/password
+# Created By: 
+# Belay Reggassa - 07/21/21
+
+# Args 1/2 - Knowledge Base API username/password
 
 import sys
 import json
@@ -18,18 +19,28 @@ payload = json.dumps({
     "u_bservice": "Knowledge Base",
     "u_display_categories": "Systems"
 })
-print(payload)
+#print(payload)
 
 url_test = "https://surfsedev.service-now.com/api/now/table/kb_knowledge"
 url_prod = "https://surf.service-now.com/api/now/table/kb_knowledge"
-JENKINS_JOB_FOLDER="/var/lib/jenkins/"
+nextBuildNumberPath = "/var/lib/jenkins/jobs/osimage-management-centos/nextBuildNumber" #"/Users/medharatnasais.karri/Desktop/bealy/nextBuildNumber"
+JENKINS_JOB_FOLDER= "/var/lib/jenkins/jobs/osimage-management-centos/builds" #"/Users/medharatnasais.karri/Desktop/bealy/builds"
 
 
-def get_console_log(full_job_name, build_number):
-    job_path = JENKINS_JOB_FOLDER + ''.join(['jobs/' + x for x in full_job_name.split("/")]) + '/builds/' + build_number + '/log'
+def get_console_log():
+    ###################
+    #Get the latest successful build 
+    ###################
+    latestBuildNumber = open(nextBuildNumberPath, 'r')
+    latestBuildNumberValue = int(latestBuildNumber.read()) - 1
+    latestBuildNumber.close()
+    #######################
+    #Get the log file from the latest build directory
+    #######################
+    job_path = JENKINS_JOB_FOLDER + ''.join('/'+ str(latestBuildNumberValue) + '/log')
+    print(job_path)
     console_text_file = open(job_path, 'r')
     file_content = console_text_file.read()
-    console_text_string = ""
     #for line in file_content:
         #line_split = re.split("{}|{}".format(PRE_STR, POST_STR), line)
        # if len(line_split) < 2:
@@ -40,38 +51,22 @@ def get_console_log(full_job_name, build_number):
     console_text_file.close()
     return file_content
 
-def get_jobinfo_from_url(job_url):
-    url_components = job_url.split("/")    # This line splits the url (string) on the backslash characters (string -> array)
-    url_components_clean = [comp for comp in url_components if comp != 'job' and comp != '']    # This line cleans the url for 'job' prefixes and empty strings
-    build_number = url_components_clean[-1]   # Build number will be the last element in the array
-    full_job_name = '/'.join([x for x in url_components_clean[2:-1]])
-    return full_job_name, build_number
+
 
 def main():
-    #knowladge Api Credentials 
-    #base_url = "https://uswpldevops01.corp.service-now.com:8443"
+    #Get the latest console log text
+    latest_build_console_text =get_console_log() #requests.get(console_text_url)
     
-    #latest_build_number = requests.get(base_url + "/job/osimage-management-centos/lastSuccessfulBuild/buildNumber"))
-    # print("response from latest build number: ", latest_build_number.text)
+    #Process the console log for the POST request 
+    html_string_for_post = latest_build_console_text.replace("\n", "<br/>").replace("","<br/>").replace('"',"'")
     
-    #console_text_url = base_url + "/job/osimage-management-centos/" + latest_build_number.text + "/consoleText"
-    # print(console_text_url)
-    job_url = sys.argv[1]
-    full_job_name, build_number = get_jobinfo_from_url(job_url)
-    job_name_no_path = full_job_name.split("/")[-1]
-    #job_console_log = 
-    latest_build_console_text =get_console_log(full_job_name, build_number) #requests.get(console_text_url)
-    # print("response from latest console text: ", latest_build_console_text.text)
-    
-    
-    html_string_for_post = latest_build_console_text.text.replace("\n", "<br/>").replace("","<br/>").replace('"',"'")
-    # print(html_string_for_post)
+    #Prepare the console log for the POST request 
     final_payload = payload.replace("<REPLACED_TEXT>", html_string_for_post)
     print(final_payload)
-    #ADM Credentials  
+    
+    #Post the request to the Knowledge API 
     response = requests.post(url_test, data=final_payload.encode('utf-8'), headers={'Content-Type': 'application/json'}, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
     print(response)
 
 if __name__ == "__main__":
     main()
-
